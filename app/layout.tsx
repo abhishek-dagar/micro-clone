1dc85d2663f5926328dac69f6ae15f1b71590a04
Abhishek Dagar <dagarabhishek584@gmail.com>
2024-02-05 20:02:46 +0530

    change in title

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Micro Clone</title>
+      </head>
       <body className={inter.className}>
         <Suspense fallback={<Loading/>}>
           <ConvexClientProvider>
